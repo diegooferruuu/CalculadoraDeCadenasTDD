4f74c6856ed252302101383c9197d9265fc8291b
Diego Ferrufino <diego.ferrufino@ucb.edu.bo>
2026-05-27 22:03:14 -0400

    Test: Deberia sumar varios numeros sumados por coma

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,9 @@
 function calcular_cadena(cadena) {
-    if (cadena === ""){
-        return 0;
-    }
-    else
-        return Number(cadena);
+    if (cadena === "") return 0;
+
+    return cadena
+        .split(",")
+        .reduce((acumulado, actual) => acumulado + Number(actual), 0);
 }
 
 export default calcular_cadena;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,11 +1,20 @@
 import calcular_cadena from "./calculadora.js";
 
 describe("Calcular", () => {
-  it("deberia devolver 0", () => {
+  it("deberia devolver 0 para cadena vacia", () => {
     expect(calcular_cadena("")).toEqual(0);
   });
+
   it("deberia devolver el numero si la cadena tiene un solo numero", () => {
     expect(calcular_cadena("5")).toEqual(5);
-    expect(calcular_cadena("12")).toEqual(12);
+  });
+
+  it("deberia sumar dos numeros separados por coma", () => {
+    expect(calcular_cadena("1,2")).toEqual(3);
+  });
+
+  it("deberia sumar varios numeros separados por coma", () => {
+    expect(calcular_cadena("1,2,3")).toEqual(6);
+    expect(calcular_cadena("1,2,4,8")).toEqual(15);
   });
 });
